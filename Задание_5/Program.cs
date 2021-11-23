cb535d21c1a50c63c6b11bc758d623fec10ae3d2
vladalexgor@ya.ru <vladalexgor@ya.ru>
2021-11-23 22:54:15 +0300

    Добавьте файлы проекта.

---
+++ Задание_5/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задание_5
+{
+    //Сформировать одномерный массив из 7 элементов. Заполнить массив числами, вводимыми с клавиатуры, определить среднее арифметическое элементов.
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите последовательно 7 чисел:");
+            int n = 7;
+            double a = 0;
+            double Sum = 0;
+            double SA = 0;
+            int[] array = new int [n];
+            for (int i = 0; i < n; i++)
+            {
+                a = Convert.ToDouble(Console.ReadLine()); 
+                Sum = Sum + a;
+            }
+            SA = Sum / n;
+            Console.WriteLine("Среднее арифметическое введенных чисел: Sср = {0,2}", SA);
+            Console.WriteLine("Для завершения нажмите любую клавишу.");
+            Console.ReadKey();
+        }
+    }
+}
